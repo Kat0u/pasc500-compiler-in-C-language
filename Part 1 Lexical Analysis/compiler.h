6fc69193abf41094a716ec834bb1dda4ca731436
Emmanouil Ioannis Katounis <84252612+Kat0u@users.noreply.github.com>
2021-05-16 02:18:17 +0300

    Add files via upload

---
+++ Part 1 Lexical Analysis/compiler.h
@@ -0,0 +1,78 @@
+/*  
+Vouklis Marios 2115021
+Katounis Emmanouil Ioannis 2115067
+Kozanis Konstantinos 2115075
+
+keywords */
+#define T_PROGRAM			1
+#define T_CONST			2
+#define T_TYPE				3
+#define T_ARRAY			4
+#define T_SET				5
+#define T_OF				6
+#define T_RECORD			7
+#define T_VAR				8
+#define T_FORWARD			9
+#define T_FUNCTION			10
+#define T_PROCEDURE			11
+#define T_INTEGER 			12
+#define T_REAL				13
+#define T_BOOLEAN			14
+#define T_CHAR				15
+#define T_BEGIN 			16
+#define T_END				17
+#define T_IF				18
+#define T_THEN				19
+#define T_ELSE				20
+#define T_WHILE			21
+#define T_DO				22
+#define T_FOR				23
+#define T_DOWNTO			24
+#define T_TO				25
+#define T_WITH				26
+#define T_READ				27
+#define T_WRITE			28
+
+// STRING //
+#define MAX_LINE 200 // Max Line Size
+#define MAX_STR_CONST 200 
+#define T_STRING 57
+#define MAX_ERRORS 5
+// ID //
+#define T_ID            	29
+//CONSTANTS//
+#define T_ICONST		30  // Integer
+#define T_BCONST		31  // Boolean
+#define T_RCONST		32  // Real
+#define T_CCONST  		33  // ASCII Characters
+
+// Real Constants //
+#define T_RHEXCONST 51
+#define T_RDECCONST 52
+#define T_RBINCONST 53
+ // Integer Constants //
+#define T_DECCONST 54
+#define T_BINCONST 55
+#define T_HEXCONST 56
+//Operators
+#define T_RELOP	34
+#define T_ADDOP	35
+#define T_OROP		36
+#define T_MULDIVANDOP	37
+#define T_NOTOP	38
+#define T_INOP		39
+
+//Other tokens
+#define T_LPAREN		40
+#define T_RPAREN  		41
+#define T_SEMI			42
+#define T_DOT			43
+#define T_COMMA		44
+#define T_EQU			45
+#define T_COLON		46
+#define T_LBRACK		47
+#define T_RBRACK		48
+#define T_ASSIGN		49
+#define T_DOTDOT		50
+#define T_EOF			0
+
